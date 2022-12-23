1d2c0733ea96d8effac3f8875ee2a88ac777e0b9
Varun Alla <varun.alla@gmail.com>
2022-12-22 18:30:23 -0800

    LC 17 Letter Combination of phone numbers version 2 with backtracking

--- Arrays/LC17_Letter_Combinations_Phone_Number.java
+++ Arrays/LC17_Letter_Combinations_Phone_Number.java
@@ -1,42 +1,29 @@
 package Arrays;
 
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 
 class LC17_Letter_Combinations_Phone_Number{
+    String[] map= new String[]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
-        HashMap<String,List<String>> map=new HashMap<>();
-        map.put("2",Arrays.asList(new String[]{"a","b","c"}));
-        map.put("3",Arrays.asList(new String[]{"d","e","f"}));
-        map.put("4",Arrays.asList(new String[]{"g","h","i"}));
-        map.put("5",Arrays.asList(new String[]{"j","k","l"}));
-        map.put("6",Arrays.asList(new String[]{"m","n","o"}));
-        map.put("7",Arrays.asList(new String[]{"p","q","r","s"}));
-        map.put("8",Arrays.asList(new String[]{"t","u","v"}));
-        map.put("9",Arrays.asList(new String[]{"w","x","y","z"}));
-        return generate(digits,map);
+        if(digits==null||digits.length()==0){
+            return new ArrayList<>();
+        }
+        ArrayList<String> result=new ArrayList<>();
+        generate(digits,0,new StringBuilder(),result);
+        return result;
     }
-    public List<String> generate(String digits,HashMap<String,List<String>> map){
-            List<String> result=new ArrayList<>();
-            if(digits.length()==1){
-                result= map.get(""+digits.charAt(0));
+    public void generate(String digits,int index,StringBuilder sb,List<String> result){
+            if(digits.length()==index){
+                result.add(sb.toString());
+                return;
             }
-            else{
-                List<String> newmap=generate(digits.substring(1,digits.length()),map);
-                List<String> src=map.get(""+digits.charAt(0));
-                HashSet<String> result_int=new HashSet<>();
-                for(String s:newmap){
-                    for(String src_s:src){
-                        result_int.add(src_s+""+s);
-                    }
-                }
-                result=new ArrayList<>(result_int);
+            String currmap=map[digits.charAt(index)-'0'];
+            for(int i=0;i<currmap.length();i++){
+                sb.append(currmap.charAt(i));
+                generate(digits,index+1,sb,result);
+                sb.setLength(sb.length()-1);
             }
-            return result;
     }
     public static void main(String[] args) {
         LC17_Letter_Combinations_Phone_Number sol=new LC17_Letter_Combinations_Phone_Number();
